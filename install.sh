86e8bc3b6de207545d6e1481baae6b0a2c32c5c5
Chris Keller <xylo04@gmail.com>
2019-12-16 08:20:44 -0700

    Add scripts from Bob Johnson's Computer Stuff
    These files appear to originate from
    https://www.bobjohnson.com/blog/adventures-with-linux-xubuntu-on-a-toughbook-cf18/,
    all credit to them. I'm just trying to group all of this together.

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ISMOD=`lsmod|grep panasonic_hbtn`
+ISMAKE=`which make`
+ISXINPUT=`which xinput`
+ISXRANDR=`which xrandr`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+if [ "$EUID" -ne 0 ]
+  then printf "Please run as root\n"
+  exit
+fi
+if [ -n "$ISMOD" ]
+  then 
+    printf "The panasonic_hbtn module is already installed.\n\n"
+    printf "Copying the rotate script to /usr/local/bin \n\n"
+    cp -i rotate /usr/local/bin
+    chmod +x /usr/local/bin/rotate
+  exit
+fi
+if [ -n "$ISMAKE" ]
+    then
+      printf "Installing Panasonic front panel button module...\n\n"
+      make clean
+      make
+      insmod panasonic-hbtn.ko
+      printf "Copying the rotate script to /usr/local/bin \n\n"
+      cp -i rotate /usr/local/bin
+      chmod +x /usr/local/bin/rotate
+    else
+      printf "\"make\" command not found!\n"
+      printf "You need to have a valid compiler installed.\n"
+      printf "You might need to install \"make\" or check to see if it's in your PATH.\n"
+    exit
+fi
+if [ ! -n "$ISXINPUT" ]
+    then  printf "You need to install \"xinput\" for the rotate script to work.\n"
+fi
+if [ ! -n "$ISXRANDR" ]
+    then  printf "You need to install \"xrandr\" for the rotate script to work.\n"
+fi
+
+ 
